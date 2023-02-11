59469bba38c8f0a93c3b460def84fea45c71bcf0
PerfectSlayer <bruce.bujon@gmail.com>
2023-02-11 09:29:56 +0100

    feat: Refactor main window

--- src/main/java/fr/hardcoding/gitspace/Main.java
+++ src/main/java/fr/hardcoding/gitspace/Main.java
@@ -1,91 +1,32 @@
 package fr.hardcoding.gitspace;
 
 import com.googlecode.lanterna.TextColor;
-import com.googlecode.lanterna.gui2.BasicWindow;
-import com.googlecode.lanterna.gui2.BorderLayout;
-import com.googlecode.lanterna.gui2.Borders;
-import com.googlecode.lanterna.gui2.Component;
 import com.googlecode.lanterna.gui2.DefaultWindowManager;
-import com.googlecode.lanterna.gui2.Direction;
 import com.googlecode.lanterna.gui2.EmptySpace;
-import com.googlecode.lanterna.gui2.Label;
-import com.googlecode.lanterna.gui2.LinearLayout;
 import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
-import com.googlecode.lanterna.gui2.Panel;
-import com.googlecode.lanterna.gui2.Separator;
-import com.googlecode.lanterna.gui2.Window;
-import com.googlecode.lanterna.gui2.WindowListenerAdapter;
-import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import fr.hardcoding.gitspace.model.Worktree;
 import fr.hardcoding.gitspace.shell.CommandException;
+import fr.hardcoding.gitspace.ui.MainWindow;
 import fr.hardcoding.gitspace.ui.WorktreeModel;
-import fr.hardcoding.gitspace.ui.WorktreeTable;
 
-import java.awt.Desktop;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.file.Path;
-import java.util.List;
-import java.util.Objects;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Main {
 
-    private static final String TITLE = "Git Space";
 
     public static void main(String[] args) {
         new Main().run();
 
     }
 
-
     private void run() {
         DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
         try (TerminalScreen screen = defaultTerminalFactory.createScreen()) {
             screen.startScreen();
-
-            // Create window to hold the panel
-            BasicWindow window = new BasicWindow(TITLE);
-
-            window.setHints(List.of(Window.Hint.FULL_SCREEN));
-
-
-
-            Panel contentPanel = new Panel(new BorderLayout());
-
             WorktreeModel model = new WorktreeModel(Path.of("."));
-            WorktreeTable table = new WorktreeTable(model);
-            contentPanel.addComponent(table, BorderLayout.Location.CENTER);
-
-            contentPanel.addComponent(buildTooltip(), BorderLayout.Location.BOTTOM);
-
-            window.setComponent(contentPanel);
-
-            window.addWindowListener(new WindowListenerAdapter() {
-                @Override
-                public void onInput(Window basePane, KeyStroke keyStroke, AtomicBoolean deliverEvent) {
-//                    System.out.println(keyStroke);
-                }
-
-                @Override
-                public void onUnhandledInput(Window basePane, KeyStroke keyStroke, AtomicBoolean hasBeenHandled) {
-                    if (Objects.equals(keyStroke.getCharacter(), 'g')) {
-                        Worktree selectedWorktree = model.get(table.getSelectedRow());
-                        if (selectedWorktree != null && selectedWorktree.pullRequest != null) {
-                            try {
-                                Desktop.getDesktop().browse(new URI(selectedWorktree.pullRequest.url()));
-                            } catch (IOException | URISyntaxException e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-                    } else if (Objects.equals(keyStroke.getCharacter(), 'q')) {
-                        window.close();
-                    }
-                }
-            });
+            MainWindow window = new MainWindow(model);
 
             // Create gui and start gui
 //            MultiWindowTextGUI gui = new MultiWindowTextGUI(screen);
@@ -96,20 +37,4 @@ public class Main {
             e.printStackTrace();
         }
     }
-
-    private Component buildTooltip() {
-        Panel panel = new Panel(new LinearLayout(Direction.HORIZONTAL));
-        panel.addComponent(new Label("(C)reate"));
-        panel.addComponent(new Separator(Direction.VERTICAL));
-        panel.addComponent(new Label("(R)ebase"));
-        panel.addComponent(new Label("(P)ush"));
-        panel.addComponent(new Label("(G)itHub"));
-        panel.addComponent(new Label("(D)elete"));
-        panel.addComponent(new Separator(Direction.VERTICAL));
-        panel.addComponent(new Label("(Q)uit"));
-        return panel.withBorder(Borders.singleLine("Actions"));
-    }
-
-
-
 }
---
+++ src/main/java/fr/hardcoding/gitspace/ui/MainWindow.java
@@ -0,0 +1,76 @@
+package fr.hardcoding.gitspace.ui;
+
+import com.googlecode.lanterna.gui2.BasicWindow;
+import com.googlecode.lanterna.gui2.BorderLayout;
+import com.googlecode.lanterna.gui2.Borders;
+import com.googlecode.lanterna.gui2.Component;
+import com.googlecode.lanterna.gui2.Direction;
+import com.googlecode.lanterna.gui2.Label;
+import com.googlecode.lanterna.gui2.LinearLayout;
+import com.googlecode.lanterna.gui2.Panel;
+import com.googlecode.lanterna.gui2.Separator;
+import com.googlecode.lanterna.gui2.Window;
+import com.googlecode.lanterna.gui2.WindowListenerAdapter;
+import com.googlecode.lanterna.input.KeyStroke;
+import fr.hardcoding.gitspace.model.Worktree;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class MainWindow extends BasicWindow {
+
+    private static final String TITLE = "Git Space";
+
+    public MainWindow(WorktreeModel model) {
+        super(TITLE);
+        setHints(List.of(Window.Hint.FULL_SCREEN));
+
+        WorktreeTable table = new WorktreeTable(model);
+
+        Panel contentPanel = new Panel(new BorderLayout());
+        contentPanel.addComponent(table, BorderLayout.Location.CENTER);
+        contentPanel.addComponent(buildTooltip(), BorderLayout.Location.BOTTOM);
+        setComponent(contentPanel);
+
+        addWindowListener(new WindowListenerAdapter() {
+            @Override
+            public void onInput(Window basePane, KeyStroke keyStroke, AtomicBoolean deliverEvent) {
+//                    System.out.println(keyStroke);
+            }
+
+            @Override
+            public void onUnhandledInput(Window basePane, KeyStroke keyStroke, AtomicBoolean hasBeenHandled) {
+                if (Objects.equals(keyStroke.getCharacter(), 'g')) {
+                    Worktree selectedWorktree = model.get(table.getSelectedRow());
+                    if (selectedWorktree != null && selectedWorktree.pullRequest != null) {
+                        try {
+                            Desktop.getDesktop().browse(new URI(selectedWorktree.pullRequest.url()));
+                        } catch (IOException | URISyntaxException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                } else if (Objects.equals(keyStroke.getCharacter(), 'q')) {
+                    close();
+                }
+            }
+        });
+    }
+
+    private Component buildTooltip() {
+        Panel panel = new Panel(new LinearLayout(Direction.HORIZONTAL));
+        panel.addComponent(new Label("(C)reate"));
+        panel.addComponent(new Separator(Direction.VERTICAL));
+        panel.addComponent(new Label("(R)ebase"));
+        panel.addComponent(new Label("(P)ush"));
+        panel.addComponent(new Label("(G)itHub"));
+        panel.addComponent(new Label("(D)elete"));
+        panel.addComponent(new Separator(Direction.VERTICAL));
+        panel.addComponent(new Label("(Q)uit"));
+        return panel.withBorder(Borders.singleLine("Actions"));
+    }
+}
